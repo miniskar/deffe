331c0e47f99c3ce6b9eee1e0bb1e2113cd4914d1
Narasinga Rao Miniskar <miniskarnr@ornl.gov>
2020-03-01 03:36:43 -0500

    Added thread object

---
+++ utils/thread_object.py
@@ -0,0 +1,50 @@
+import threading
+import time
+
+class ThreadObject:
+    """ 
+    The Run() method will run in the background forever until the Stop() method is called
+    """
+
+    def __init__(self, method, arguments, start_flag=True):
+        """ Constructor
+        :type interval: int
+        :param interval: Check interval, in seconds
+        """
+        self.stop_thread = False
+        self.method = method
+        self.arguments = arguments
+        self.thread = threading.Thread(target=self.Run, args=())
+        self.thread.daemon = True                            # Daemonize thread
+        if start_flag:
+            self.Start()
+
+    def Start(self):
+        self.thread.start()
+
+    def Run(self):
+        """ Thread runs forever """
+        while not self.stop_thread:
+            # Do something
+            print('Doing something imporant in the background')
+            self.method(*self.arguments)
+
+    def Join(self):
+        self.thread.join()
+
+    def Stop(self, join_flag=True):
+        self.stop_thread = True
+        if join_flag:
+            self.Join()
+
+def example_run(arg1, arg2):
+    print("Arg1: "+str(arg1)+" Arg2:"+str(arg2))
+    time.sleep(1)
+    
+if __name__ == "__main__":
+    example = ThreadObject(example_run, ("Hello", "World"))
+    time.sleep(3)
+    print('Checkpoint')
+    time.sleep(4)
+    print('Bye')
+    example.Stop()
