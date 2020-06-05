62d8635e13d3c6bee88f3d2083a6e0b1a0638ec3
Narasinga Rao Miniskar <nqx@ornl.gov>
2020-06-05 14:44:08 -0400

    Added batch

--- test/run_deffe.sh
+++ test/run_deffe.sh
@@ -1,1 +1,1 @@
-python3 $DEFFE_DIR/framework/run_deffe.py -no-slurm -no-train -batch-size 1 -config config.json -init-batch-samples 1 $@
+python3 $DEFFE_DIR/framework/run_deffe.py -no-slurm -no-train -batch-size 1 -config config.json -init-batch-samples 10 -batch-size 10 $@
