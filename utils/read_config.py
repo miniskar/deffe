49579983cc1187a6d8d30328b1cc5f030bf78415
Narasinga Rao Miniskar <nqx@ornl.gov>
2020-06-17 11:50:30 -0400

    Fixed an issue in merging two jsons

--- utils/read_config.py
+++ utils/read_config.py
@@ -265,8 +265,8 @@ class DeffeConfig:
                         yield(k, dict1[k] + dict2[k])
                     else:
                         # If one of the values is not a dict, you can't continue merging it.
-                        # Value from second dict overrides one in first and we move on.
-                        yield (k, dict2[k])
+                        # Value from first dict overrides one in first and we move on.
+                        yield (k, dict1[k])
                     # Alternatively, replace this with exception raiser to alert you of value conflicts
                 elif k in dict1:
                     yield (k, dict1[k])
