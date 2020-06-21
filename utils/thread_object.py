f8d3c2ff51515f61f074b495c8500925ad68cbad
Narasinga Rao Miniskar <nqx@ornl.gov>
2020-06-20 20:51:07 -0400

    Working on process networks

--- utils/thread_object.py
+++ utils/thread_object.py
@@ -8,21 +8,21 @@
 # Date:        Apr, 2020
 # **************************************************************************
 ###
-import threading
+import threading, queue
 import time
 from queue import *
 import pdb
 
 class ThreadObject:
     """ 
-    The Run() method will run in the background forever until the Stop() method is called
+    The RunThread() method will run in the background forever until the StopThread() method is called
     """
 
     def __init__(self):
         print("ThreadObject init")
         None
 
-    def Init(self, method, arguments, start_flag=False):
+    def InitThread(self, method, arguments, start_flag=False):
         """ Constructor
         :type interval: int
         :param interval: Check interval, in seconds
@@ -35,22 +35,22 @@ class ThreadObject:
         self.stop_thread = False
         self.method = method
         self.arguments = arguments
-        self.thread = threading.Thread(target=self.Run, args=())
+        self.thread = threading.Thread(target=self.RunThread, args=())
         self.thread.daemon = True  # Daemonize thread
         self.thread_status = self.INIT
         if start_flag:
-            self.Start()
+            self.StartThread()
 
     def Connect(name, source, dest):
         queue = Queue()
         source.out_ports[name] = queue
         dest.in_ports[name] = queue
 
-    def Start(self):
+    def StartThread(self):
         self.thread_status = self.RUN
         self.thread.start()
 
-    def Run(self):
+    def RunThread(self):
         """ Thread runs forever """
         while not self.stop_thread:
             # Do something
@@ -58,33 +58,33 @@ class ThreadObject:
             self.method(*self.arguments)
         self.thread_status = self.END
 
-    def Join(self):
+    def JoinThread(self):
         if self.thread_status == self.RUN:
             self.thread.join()
 
-    def Stop(self, join_flag=True):
+    def StopThread(self, join_flag=True):
         self.stop_thread = True
         if join_flag:
-            self.Join()
+            self.JoinThread()
 
 class Producer(ThreadObject):
     def __init__(self, tag):
         self.tag = tag
-        ThreadObject.Init(self, self.run, ())
+        ThreadObject.InitThread(self, self.run, ())
 
     def run(self):
         for i in range(10):
-            self.out_ports['samples'].Enqueue(i+100)
+            self.out_ports['samples'].put(i+100, True)
             print(self.tag+": Sent data:"+str(i+100))
 
 class Consumer(ThreadObject):
     def __init__(self, tag):
         self.tag = tag
-        ThreadObject.Init(self, self.run, ())
+        ThreadObject.InitThread(self, self.run, ())
 
     def run(self):
         for i in range(10):
-            data = self.in_ports['samples'].Dequeue()
+            data = self.in_ports['samples'].get(True)
             print(self.tag+": Received data:"+str(data))
 
 
@@ -92,11 +92,11 @@ if __name__ == "__main__":
     example1 = Producer("Prod")
     example2 = Consumer("Cons")
     ThreadObject.Connect("samples", example1, example2)
-    example2.Start()
-    example1.Start()
+    example2.StartThread()
+    example1.StartThread()
     time.sleep(3)
     print("Checkpoint")
     time.sleep(4)
     print("Bye")
-    example1.Stop()
-    example2.Stop()
+    example1.StopThread()
+    example2.StopThread()
